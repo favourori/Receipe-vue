894ba86476fce471672e8907d5f489969c3815e4
Favour Ori <orifavour2000@gmail.com>
2019-10-13 11:20:42 +0200

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav class="light-blue darken-4 z-depth-0">
+      <div class="nav-wrapper container">
+        <a href="#" class="brand-logo"><img src="@/assets/recipe-book.png" width="45"></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Add recipe</a>
+          </li>
+                <li>
+            <a href="sass.html">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+.brand-logo > span,.brand-logo > img{
+   vertical-align: middle;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+ <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus nam veritatis fugiat soluta iusto animi atque perspiciatis, similique velit! Officia!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
